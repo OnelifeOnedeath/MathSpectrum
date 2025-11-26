44f1ccf39b23d6dbfd41cb448492f792adac68ec
OnelifeOnedeath <137838121+OnelifeOnedeath@users.noreply.github.com>
2025-11-26 18:39:12 +0300

    Update Harmonic.cs

--- MathSpectrum.Core/Models/Harmonic.cs
+++ MathSpectrum.Core/Models/Harmonic.cs
@@ -1,1 +1,9 @@
+namespace MathSpectrum.Core.Models;
 
+public class Harmonic
+{
+    public int Order { get; set; }           // Порядок гармоники (1, 3, 5...)
+    public double Amplitude { get; set; }    // Амплитуда
+    public double Frequency { get; set; }    // Частота
+    public double Phase { get; set; }        // Фаза
+}
