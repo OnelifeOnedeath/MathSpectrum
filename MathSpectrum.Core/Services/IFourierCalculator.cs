657e6befd9be71f0d60fd1dc07aa2b0568e0aeb6
OnelifeOnedeath <137838121+OnelifeOnedeath@users.noreply.github.com>
2025-11-26 18:39:51 +0300

    Update IFourierCalculator.cs

--- MathSpectrum.Core/Services/IFourierCalculator.cs
+++ MathSpectrum.Core/Services/IFourierCalculator.cs
@@ -1,1 +1,7 @@
+namespace MathSpectrum.Core.Services;
 
+public interface IFourierCalculator
+{
+    List<Harmonic> CalculateHarmonics(FunctionType functionType, int maxHarmonics, double period = 2 * Math.PI);
+    double CalculateFunctionValue(FunctionType functionType, double x, double period = 2 * Math.PI);
+}
