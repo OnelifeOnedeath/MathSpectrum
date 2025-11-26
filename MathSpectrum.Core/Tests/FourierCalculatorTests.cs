7c35e730720ea2562f64b5f408c55f50e557680a
OnelifeOnedeath <137838121+OnelifeOnedeath@users.noreply.github.com>
2025-11-26 18:44:40 +0300

    Create FourierCalculatorTests.cs

---
+++ MathSpectrum.Core/Tests/FourierCalculatorTests.cs
@@ -0,0 +1,13 @@
+public class FourierCalculatorTests
+{
+    [Theory]
+    [InlineData(FunctionType.SquareWave, 1, 4/Math.PI)]
+    [InlineData(FunctionType.SquareWave, 3, 4/(3*Math.PI))]
+    public void CalculateHarmonics_ReturnsCorrectAmplitudes(FunctionType type, int order, double expectedAmplitude)
+    {
+        var calculator = new FourierCalculator();
+        var harmonics = calculator.CalculateHarmonics(type, order);
+        
+        Assert.Equal(expectedAmplitude, harmonics.Last().Amplitude, 0.001);
+    }
+}
