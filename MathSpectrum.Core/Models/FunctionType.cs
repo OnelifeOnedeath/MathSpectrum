7ec7010ae843203fe78168ab84d5d20bea83b2c4
OnelifeOnedeath <137838121+OnelifeOnedeath@users.noreply.github.com>
2025-11-26 18:38:58 +0300

    Update FunctionType.cs

--- MathSpectrum.Core/Models/FunctionType.cs
+++ MathSpectrum.Core/Models/FunctionType.cs
@@ -1,1 +1,9 @@
+namespace MathSpectrum.Core.Models;
 
+public enum FunctionType
+{
+    SquareWave,     // Прямоугольная волна
+    TriangleWave,   // Треугольная волна  
+    SawtoothWave,   // Пилообразная волна
+    Custom          // Пользовательская функция
+}
